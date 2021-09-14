cac920d2b6cde753ed5f45f67e61e780017e53ce
pritomus <66831544+pritomus@users.noreply.github.com>
2021-09-14 22:21:50 +0200

    Rockpaperscissors file
    rock, paper, scissors

---
+++ rockpaperscissors.py
@@ -0,0 +1,28 @@
+print("rock")
+print("paper")
+print("scissors")
+
+player_one =  input("PLayer1 , make your move").lower()
+
+import random
+autobot = random.randint(0, 3)
+if autobot == 0:
+    computer = "rock"
+elif autobot == 1:
+    computer = "paper"
+else:
+    computer = "scissors"
+print(f"Autobot Choosed : {computer}" )
+
+
+if player_one == computer:
+        print("you both have " +  player_one + "Play again")
+elif player_one == "rock" and computer == "scissors":
+        print("Player one wins")
+elif player_one == "paper" and computer == "rock":
+        print("Player one wins")
+elif player_one == "scissors" and computer == "paper":
+        print("player one wins ")
+else:
+    print("Computer wins")			
+				
